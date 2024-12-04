80b9c6736a458198108f32dc70d5a9725b720bcb
MasroorHaider1 <146111560+MasroorHaider1@users.noreply.github.com>
2024-12-03 19:52:08 -0500

    Add files via upload

--- Day2.java
+++ Day2.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Day2 {
@@ -13,20 +11,16 @@ public class Day2 {
 
     }
 
-
-
     public static Integer getAnswer(ArrayList<String> fileData) {
-       int safe=0;
+        int safe = 0;
 
-       //for each line of filedata, we need to convert that to an integer list
-        for(String line:fileData){
+        //for each line of filedata, we need to convert that to an integer list
+        for (String line : fileData) {
 
-
-            ArrayList<Integer>temporaryLineList=new ArrayList<Integer>();
-
+            ArrayList<Integer> temporaryLineList = new ArrayList<Integer>();
 
             //makes string array of A LINE
-            String[] stringSplitLineList=line.split(" ");
+            String[] stringSplitLineList = line.split(" ");
 
             //makes temporaryLineList
             for (String s : stringSplitLineList) {
@@ -34,29 +28,19 @@ public class Day2 {
                 temporaryLineList.add(eachLineNumber);
             }
 
-            for(int i=0;i<temporaryLineList.size();i++){
-                int firstNumber=temporaryLineList.get(i);
-                if(i==temporaryLineList.size()-1){
-                    int secondNumber=-1000;
-
-                }else{
-                    int secondNumber=temporaryLineList.get(i+1);
-                }
-
-
-
+            //check if line good
+            if (isSafe(temporaryLineList)) {
+                safe++;
             }
 
-
-
-
-
-
-
-
-
         }
+
+
+
+        //part 1
         return safe;
+
+
     }
 
 
@@ -64,6 +48,37 @@ public class Day2 {
 
 
 
+    public static boolean isSafe(ArrayList<Integer> temporaryLineList) {
+
+        //used Mr.Das strat of checking if all is negative or positive
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        //for each difference in the list, I have to check if it was like before, -1 because it could be odd
+        for (int i = 0; i < temporaryLineList.size() - 1; i++) {
+            int diff = temporaryLineList.get(i + 1) - temporaryLineList.get(i);
+
+            //if the difference is out of range, no good, automatically gone
+            if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+                return false;
+            }
+
+            //if a positive difference shows, it cant be decreasing
+            if (diff > 0) {
+                isDecreasing = false;
+            }
+
+            //if a negative difference shows, it cant be increasing
+            if (diff < 0) {
+                isIncreasing = false;
+            }
+
+            // FOR EVERY TEST, IT SHOULD ONLY CONSISTTENLY KEEP ONE BEING FALSE, IF BOTH FALSE, THEN NO.
+        }
+
+        //return true only if  all increasing or all decreasing
+        return isIncreasing || isDecreasing;
+    }
 
 
 
@@ -79,13 +94,8 @@ public class Day2 {
                     fileData.add(line);
             }
             return fileData;
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             return fileData;
         }
     }
-
-
-
-
 }
