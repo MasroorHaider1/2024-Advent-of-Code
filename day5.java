bb71bff9d7c7eb08cc4ed76e92ad69d7e17364eb
MasroorHaider1 <146111560+MasroorHaider1@users.noreply.github.com>
2024-12-10 19:48:56 -0500

    Add files via upload

--- day5.java
+++ day5.java
@@ -1,56 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Day5 {
+public class day5 {
     public static void main(String[] args) {
-
         ArrayList<String> fileData = getFileData("src/day5.txt");
         System.out.println(getAnswer1(fileData));
-
     }
 
-    public static String getAnswer1(ArrayList<String> fileData) {
-        ArrayList<Integer> ruleList = new ArrayList<Integer>();
-        ArrayList<Integer> unsortedPageList = new ArrayList<Integer>();
+    public static int getAnswer1(ArrayList<String> fileData) {
+        // the input looks like 2d arrays so I have to make 2d arrays and not just one array
+        List<int[]> ruleList = new ArrayList<>();
+        List<int[]> unsortedPageList = new ArrayList<>();
 
+        int position = 0;
 
-        //set position as 0 for now
-        int position=0;
-        //get data list of all rule orders, and do this until there is a gap in the data list
-        do{
-
-            //split each number in each line
+        //while every line has | in it
+        while (fileData.get(position).contains("|")) {
             String[] stringSplitLineList = fileData.get(position).split("\\|");
-            //in each temp array string thing
-            for (String s : stringSplitLineList) {
-                int eachLineNumber = Integer.parseInt(s);
-                ruleList.add(eachLineNumber);
-            }
+
+            //every rule is now a 2d array spot
+            int[] rule = new int[]{Integer.parseInt(stringSplitLineList[0]), Integer.parseInt(stringSplitLineList[1])};
+            //add it to the rule list
+            ruleList.add(rule);
             position++;
+        }
 
-        }while(fileData.get(position).contains("|"));
 
 
-        //MAKE WHATEVER THIS IS HAVE A 2D ARRAY
+        // while every line has a comma in it, add each row with an array of nummbers in it THIS IS AFTER THE POSITION IS FINALIZED
+        for (int i = position; i < fileData.size(); i++) {
 
-        //set position as 0 for now
-        int position2=0;
-        //get data list of all rule orders, and do this until there is a gap in the data list
-        do{
-
-            //split each number in each line
-            String[] stringSplitLineList = fileData.get(position).split(",");
-            //in each temp array string thing
-            for (String s : stringSplitLineList) {
-                int eachLineNumber = Integer.parseInt(s);
-                ruleList.add(eachLineNumber);
+            //each row is now array
+            String[] stringSplitLineList = fileData.get(i).split(",");
+            //array update now is a line length
+            int[] update = new int[stringSplitLineList.length];
+            //for each row I now have to put a spot from that line into update line spot
+            for (int j = 0; j < stringSplitLineList.length; j++) {
+                update[j] = Integer.parseInt(stringSplitLineList[j]);
             }
-            position++;
-
-        }while(fileData.get(position2).contains(","));
-
+            unsortedPageList.add(update);
+        }
 
 
 
@@ -59,18 +51,10 @@ public class Day5 {
 
 
 
-
-
-
-
-
-
+    //get the middle of each line, it is odd
+    public static int findMiddlePage(int[] update) {
+        return update[update.length / 2];
     }
-
-
-
-
-
 
 
 
@@ -82,29 +66,19 @@ public class Day5 {
 
 
     public static ArrayList<String> getFileData(String fileName) {
-        ArrayList<String> fileData = new ArrayList<String>();
+        ArrayList<String> fileData = new ArrayList<>();
         try {
             File f = new File(fileName);
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String line = s.nextLine();
-                if (!line.equals(""))
+                if (!line.isEmpty())
                     fileData.add(line);
             }
-            return fileData;
+            s.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileName);
         }
-        catch (FileNotFoundException e) {
-            return fileData;
-        }
+        return fileData;
     }
-
-
-
-
-
-
-
-
-
-
 }
