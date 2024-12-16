2a215643327b93a714bc79920575012e26555241
MasroorHaider1 <146111560+MasroorHaider1@users.noreply.github.com>
2024-12-16 14:13:53 -0500

    Add files via upload

--- day5.java
+++ day5.java
@@ -10,112 +10,89 @@ public class day5 {
     }
 
     public static int getAnswer1(ArrayList<String> fileData) {
-        // the input looks like 2d arrays so I have to make 2d arrays and not just one array
         ArrayList<int[]> ruleList = new ArrayList<>();
         ArrayList<int[]> unsortedPageList = new ArrayList<>();
         ArrayList<int[]> passedRulePageList = new ArrayList<>();
 
         int position = 0;
 
-
-        //[[x,y],[x2,y2]
-        //while every line has | in it
+        // Parse the page ordering rules
         while (fileData.get(position).contains("|")) {
             String[] stringSplitLineList = fileData.get(position).split("\\|");
 
-            //every rule is now a 2d array spot
+            // Each rule is represented as an array [X, Y]
             int[] rule = new int[]{Integer.parseInt(stringSplitLineList[0]), Integer.parseInt(stringSplitLineList[1])};
-            //add it to the rule list
             ruleList.add(rule);
             position++;
         }
 
-
-        //[[a,b,c,d],[a2,b2,c2,d2]]
-        // while every line has a comma in it, add each row with an array of nummbers in it THIS IS AFTER THE POSITION IS FINALIZED
+        // Parse the update pages
         for (int i = position; i < fileData.size(); i++) {
-
-            //each row is now array
             String[] stringSplitLineList = fileData.get(i).split(",");
-            //array update now is a line length
             int[] update = new int[stringSplitLineList.length];
-            //for each row I now have to put a spot from that line into update line spot
+
             for (int j = 0; j < stringSplitLineList.length; j++) {
                 update[j] = Integer.parseInt(stringSplitLineList[j]);
             }
             unsortedPageList.add(update);
         }
 
+        // Check if each update follows the rules
+        for (int i = 0; i < unsortedPageList.size(); i++) {
+            boolean listPassed = true;
+            int[] currentUpdate = unsortedPageList.get(i);
 
+            for (int j = 0; j < currentUpdate.length; j++) {
+                for (int k = 0; k < ruleList.size(); k++) {
+                    int lessThan = ruleList.get(k)[0];
+                    int greaterThan = ruleList.get(k)[1];
 
-
-
-
-
-
-        //check if it passes the rule check,NO NEED TO SORT IT OUT:
-
-        // check every unsortedRuleList
-        for(int i=0;i<unsortedPageList.size();i++ ){
-
-            //inital state of being true
-            boolean listPassed=true;
-
-            //for every list, we need to check each spot
-            for (int x=0; x < (unsortedPageList.get(i)).length ;x++){
-                //now I have to check each rule, and confirm it. if it is not then no add, or else add
-
-                //loop through each rule list:
-                for(int y=0;y<ruleList.size();y++){
-                    //make temporary variables
-                    int lessThanPlaceInList=ruleList.get(y)[0];
-                    int greaterThanPlaceInList=ruleList.get(y)[1];
-                    int  currentSpot=unsortedPageList.get(i)[x];
-                    if(currentSpot!=lessThanPlaceInList||currentSpot!=greaterThanPlaceInList){
-                        continue;
-                    }else{
-
-                        //NOW ACTUALLY CHECK THE RULE
-                        if(currentSpot==(lessThanPlaceInList) && !listPassed){
-                            //if statement if the current spot is a greaterThan placelist and check if it is grater then the lessthan place list, or else false
-                            if(unsortedPageList.indexOf(currentSpot)<unsortedPageList.indexOf(greaterThanPlaceInList)){
-                                listPassed=true;
-                            }else{
-                                listPassed=false;
-                                continue;
-                            }
-                        }else{
-                            //if statement is a less than spot, check if it is less than the grater spot, or else false
-                            if(unsortedPageList.indexOf(currentSpot)>unsortedPageList.indexOf(lessThanPlaceInList)&&listPassed==false){
-                                listPassed=true;
-                            }else{
-                                listPassed=false;
-                                continue;
-                            }
-                        }
-                    }
-                    if(listPassed==true){
-                        passedRulePageList.add(unsortedPageList.get(i));
+                    // Check if the current page violates any rules
+                    if (containsPages(currentUpdate, lessThan, greaterThan) && !isValidOrder(currentUpdate, lessThan, greaterThan)) {
+                        listPassed = false;
+                        break;
                     }
                 }
-
+                if (!listPassed) break;
             }
 
+            // If the update passed all rules, add it to the passed list
+            if (listPassed) {
+                passedRulePageList.add(currentUpdate);
+            }
         }
 
-
-
-        int middleValues=0;
-        for(int i=0;i<passedRulePageList.size();i++){
-            middleValues+=  passedRulePageList.get(i)[(passedRulePageList.get(i).length)/2];
+        // Calculate the middle values
+        int middleValues = 0;
+        for (int[] passedUpdate : passedRulePageList) {
+            middleValues += passedUpdate[passedUpdate.length / 2];
         }
+
         return middleValues;
     }
 
+    // Check if the array contains both pages
+    public static boolean containsPages(int[] array, int lessThan, int greaterThan) {
+        boolean foundLessThan = false;
+        boolean foundGreaterThan = false;
+        for (int value : array) {
+            if (value == lessThan) foundLessThan = true;
+            if (value == greaterThan) foundGreaterThan = true;
+            if (foundLessThan && foundGreaterThan) return true;
+        }
+        return false;
+    }
 
-
-
-
+    // Check if the order is valid according to the rule
+    public static boolean isValidOrder(int[] array, int lessThan, int greaterThan) {
+        int indexLessThan = -1;
+        int indexGreaterThan = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == lessThan) indexLessThan = i;
+            if (array[i] == greaterThan) indexGreaterThan = i;
+        }
+        return indexLessThan < indexGreaterThan;
+    }
 
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<>();
@@ -124,8 +101,7 @@ public class day5 {
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String line = s.nextLine();
-                if (!line.isEmpty())
-                    fileData.add(line);
+                if (!line.isEmpty()) fileData.add(line);
             }
             s.close();
         } catch (FileNotFoundException e) {
@@ -133,10 +109,4 @@ public class day5 {
         }
         return fileData;
     }
-
-
-
-
-
-
 }
