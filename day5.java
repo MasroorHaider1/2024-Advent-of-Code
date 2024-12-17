c76cf1697b52ba3269760286a82177b69f62420b
MasroorHaider1 <146111560+MasroorHaider1@users.noreply.github.com>
2024-12-17 14:49:58 -0500

    Add files via upload

--- day5.java
+++ day5.java
@@ -1,65 +1,64 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class day5 {
+
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/day5.txt");
-        System.out.println(getAnswer1(fileData));
+        String result = getAnswers(fileData);
+        System.out.println(result);
+    }
+
+    public static String getAnswers(ArrayList<String> fileData) {
+        // Get the answers for Part 1 and Part 2
+        int part1Answer = getAnswer1(fileData);
+        int part2Answer = getAnswer2(fileData);
+
+        // Combine the results into a single string
+        return "Part 1: " + part1Answer + ", Part 2: " + part2Answer;
     }
 
     public static int getAnswer1(ArrayList<String> fileData) {
         ArrayList<int[]> ruleList = new ArrayList<>();
         ArrayList<int[]> unsortedPageList = new ArrayList<>();
         ArrayList<int[]> passedRulePageList = new ArrayList<>();
-        ArrayList<int[]> notPassedRulePageList=new ArrayList<>();
 
         int position = 0;
 
-        // add the page ordering rules
+        // Add the page ordering rules
         while (fileData.get(position).contains("|")) {
             String[] stringSplitLineList = fileData.get(position).split("\\|");
-
-            // Each rule is represented as an array [X, Y]
             int[] rule = new int[]{Integer.parseInt(stringSplitLineList[0]), Integer.parseInt(stringSplitLineList[1])};
             ruleList.add(rule);
             position++;
         }
 
-        // add the update pages
+        // Add the update pages
         for (int i = position; i < fileData.size(); i++) {
             String[] stringSplitLineList = fileData.get(i).split(",");
             int[] update = new int[stringSplitLineList.length];
-
             for (int j = 0; j < stringSplitLineList.length; j++) {
                 update[j] = Integer.parseInt(stringSplitLineList[j]);
             }
             unsortedPageList.add(update);
         }
 
-
-
-
         // Check if each update follows the rules
-        for (int i = 0; i < unsortedPageList.size(); i++) {
+        for (int[] currentUpdate : unsortedPageList) {
             boolean listPassed = true;
-            int[] currentUpdate = unsortedPageList.get(i);
+            for (int[] rule : ruleList) {
+                int lessThan = rule[0];
+                int greaterThan = rule[1];
 
-            for (int j = 0; j < currentUpdate.length; j++) {
-                for (int k = 0; k < ruleList.size(); k++) {
-                    int lessThan = ruleList.get(k)[0];
-                    int greaterThan = ruleList.get(k)[1];
-
-                    // Check if the current page violates any rules and add it to not passed list
-                    if (containsPages(currentUpdate, lessThan, greaterThan) && !isValidOrder(currentUpdate, lessThan, greaterThan)) {
-                        listPassed = false;
-                        notPassedRulePageList.add(currentUpdate);
-                        break;
-                    }
+                // Check if the current page violates any rules
+                if (containsPages(currentUpdate, lessThan, greaterThan) &&
+                        !isValidOrder(currentUpdate, lessThan, greaterThan)) {
+                    listPassed = false;
+                    break;
                 }
-                if (!listPassed) break;
             }
 
             // If the update passed all rules, add it to the passed list
@@ -77,79 +76,89 @@ public class day5 {
         return middleValues;
     }
 
+    public static int getAnswer2(ArrayList<String> fileData) {
+        ArrayList<int[]> ruleList = new ArrayList<>();
+        ArrayList<int[]> unsortedPageList = new ArrayList<>();
+        ArrayList<int[]> notPassedRulePageList = new ArrayList<>();
+
+        int position = 0;
+
+        // Add the page ordering rules
+        while (fileData.get(position).contains("|")) {
+            String[] stringSplitLineList = fileData.get(position).split("\\|");
+            int[] rule = new int[]{Integer.parseInt(stringSplitLineList[0]), Integer.parseInt(stringSplitLineList[1])};
+            ruleList.add(rule);
+            position++;
+        }
+
+        // Add the update pages
+        for (int i = position; i < fileData.size(); i++) {
+            String[] stringSplitLineList = fileData.get(i).split(",");
+            int[] update = new int[stringSplitLineList.length];
+            for (int j = 0; j < stringSplitLineList.length; j++) {
+                update[j] = Integer.parseInt(stringSplitLineList[j]);
+            }
+            unsortedPageList.add(update);
+        }
+
+        // Check if each update does  not follow the rules
+        for (int[] currentUpdate : unsortedPageList) {
+            boolean listPassed = true;
+            for (int[] rule : ruleList) {
+                int lessThan = rule[0];
+                int greaterThan = rule[1];
+
+                // Check if the current page violates any rules
+                if (containsPages(currentUpdate, lessThan, greaterThan) &&
+                        !isValidOrder(currentUpdate, lessThan, greaterThan)) {
+                    listPassed = false;
+                    notPassedRulePageList.add(currentUpdate);
+                    break;
+                }
+            }
+        }
+
+        // Sort incorrectly ordered lists and calculate middle values
+        int middleValuesSum = 0;
+        for (int[] incorrectUpdate : notPassedRulePageList) {
+           
+            middleValuesSum += incorrectUpdate[incorrectUpdate.length / 2];
 
 
-    //get the incorrect lists sorted and then add the middle values:
 
-    //if the greater than place is less than the actual, just swap places.
+        }
+
+        return middleValuesSum;
+    }
 
 
 
 
-    // Check if the array contains both page
+    // Check if the array contains both pages
     public static boolean containsPages(int[] array, int lessThan, int greaterThan) {
         boolean foundLessThan = false;
         boolean foundGreaterThan = false;
         for (int value : array) {
-            if (value == lessThan) {
-                foundLessThan = true;
-            }
-            if (value == greaterThan){
-                foundGreaterThan = true;
-            }
-            if (foundLessThan && foundGreaterThan){
-                return true;
-            }
+            if (value == lessThan) foundLessThan = true;
+            if (value == greaterThan) foundGreaterThan = true;
+            if (foundLessThan && foundGreaterThan) return true;
         }
         return false;
     }
+
 
     // Check if the order is valid
     public static boolean isValidOrder(int[] array, int lessThan, int greaterThan) {
         int indexLessThan = -1;
         int indexGreaterThan = -1;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == lessThan){
-                indexLessThan = i;
-            }
-            if (array[i] == greaterThan) {
-                indexGreaterThan = i;
-            }
+            if (array[i] == lessThan) indexLessThan = i;
+            if (array[i] == greaterThan) indexGreaterThan = i;
         }
         return indexLessThan < indexGreaterThan;
     }
 
-
-    // Check if the order is valid and fix the list
-    public static void isValidOrderFromWrongList(int[] array, int lessThan, int greaterThan) {
-        int indexLessThan = -1;
-        int indexGreaterThan = -1;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == lessThan){
-                indexLessThan = i;
-            }
-            if (array[i] == greaterThan) {
-                indexGreaterThan = i;
-            }
-        }
-        boolean status=indexLessThan > indexGreaterThan;
-
-        if(status){
-            //NOW SWITCH PLACES
-
-            Collections.swap(array, array.//(indexLessThan), array.//(indexGreaterThan));
-
-        }
-
-    }
-
-
-
-
-
-
-
-
+    // Read file data
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<>();
         try {
@@ -166,3 +175,4 @@ public class day5 {
         return fileData;
     }
 }
+
